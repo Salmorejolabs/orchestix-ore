7f914cd4d8a2667f05442a64a0e4199505b66fb0
SalmorejoLabs <info@salmorejolabs.com>
2026-04-15 21:59:07 +0200

    Update branch_manager.py

--- scheduler/branch_manager.py
+++ scheduler/branch_manager.py
@@ -1,1 +1,10 @@
+class ForkManager:
+    """Gestiona la creación de ramas fractales (forks) del pensamiento."""
+
+    def create_fork(self, parent_branch: str, new_branch: str):
+        return {
+            "parent": parent_branch,
+            "child": new_branch,
+            "status": "created"
+        }
 
