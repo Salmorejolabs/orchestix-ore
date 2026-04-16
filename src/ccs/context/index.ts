a66d29a5e05ef4c2071613d0dd32c7d92ada14b5
SalmorejoLabs <info@salmorejolabs.com>
2026-04-16 10:45:44 +0200

    Create index.ts

---
+++ src/ccs/context/index.ts
@@ -0,0 +1,14 @@
+import { loadConstitution } from "../constitution";
+import { Jurisprudence } from "../jurisprudence";
+
+export type CcsContext = {
+  constitution: ReturnType<typeof loadConstitution>;
+  jurisprudence: Jurisprudence;
+};
+
+export function createContext(): CcsContext {
+  return {
+    constitution: loadConstitution(),
+    jurisprudence: new Jurisprudence()
+  };
+}
